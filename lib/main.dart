d8fbd1279cdf2d98acca9b0dff24f6ae4f2f2b3c
Xin Ying Lim <springflight@gmail.com>
2018-12-08 02:12:58 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'testObjects.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+
+
+  Widget buildListView(){
+
+    // return Center(
+    //           child: FadeInImage.memoryNetwork(
+    //             placeholder: kTransparentImage,
+    //             image:
+    //                 'https://github.com/flutter/website/blob/master/src/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+    //           ),
+    //         );
+
+
+      return StreamBuilder(
+        stream: Firestore.instance.collection("fruits").snapshots(),
+        builder: (context, AsyncSnapshot asyncSnap){
+          if (!asyncSnap.hasData) return CircularProgressIndicator();
+          QuerySnapshot query = asyncSnap.data;
+          List<Fruit> fruitList = query.documents.map((doc) => Fruit.fromSnap(doc)).toList();
+          if (fruitList.isEmpty) {return Center(child: Text("All out"));}
+          return ListView.builder(
+        itemCount: fruitList.length,
+        itemBuilder: (context, index){
+          return FruitCard(fruitList[index]);
+        },
+      );
+        },
+      );
+      
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // initializeStream();
+
+    
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 400.0,
+            child: buildListView(),
+          )
+        ]
+      )
+    );
+
+    
+  }
+
+}
+
+class FruitCard extends StatefulWidget {
+  Fruit fruit;
+  FruitCard(this.fruit);
+  @override
+  _FruitCardState createState() => _FruitCardState();
+}
+
+class _FruitCardState extends State<FruitCard> {
+
+Widget _showProfilePicture() {
+    return FadeInImage.memoryNetwork(
+      placeholder: kTransparentImage,
+      image: widget.fruit.image,
+      fit: BoxFit.cover,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child :
+        Column(
+          children: [
+            // _showProfilePicture,
+            ListTile(
+              title: Text(widget.fruit.fruitName),
+              trailing: Text("${widget.fruit.qty} pcs"),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    setState((){
+                      widget.fruit.qty += 1;
+                      TestObjects().fruits.forEach((fruit){
+                          debugPrint(fruit.toString());
+                      });
+                      });
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: ((){
+                    setState(() {
+                      widget.fruit.qty -= 1;
+                      TestObjects().fruits.forEach((fruit){
+                          debugPrint(fruit.toString());
+                      });});
+                  }),
+                )
+              ],
+            )
+          ]
+        ),
+    );
+  }
+}
---
+++ lib/testObjects.dart
@@ -0,0 +1,95 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+class TestObjects {
+  static final TestObjects _instance = new TestObjects._();
+
+  static get instance => _instance;
+  TestObjects._() {
+    // initializeFirestore();
+//    fruits = [
+//      Fruit("apple", "apple", 2),
+//      Fruit("orange", "orange", 4),
+//      Fruit("pear", "pear", 6),
+//    ];
+  }
+
+  TestObjects();
+
+  Stream<List<Fruit>> fruitStream = Fruit.colRef.snapshots().map((querySnap){
+      List<DocumentSnapshot> docList = querySnap.documents;
+      return docList.map((docSnap) => Fruit.fromSnap(docSnap));
+    });
+
+  void initializeFirestore(){
+    this.fruitStream = Fruit.colRef.snapshots().map((querySnap){
+      List<DocumentSnapshot> docList = querySnap.documents;
+      return docList.map((docSnap) => Fruit.fromSnap(docSnap));
+    });
+  }
+  
+    List<Fruit> fruits = [];
+
+  void addNewFruit(Fruit fruit) {
+    this.fruits.add(fruit);
+  }
+}
+
+class Fruit {
+  static CollectionReference colRef = Firestore.instance.collection("fruits");
+  //todo include collection reference to pictures for card view
+  DocumentReference get ref => colRef.document(id);
+
+  String fruitNameFS = "fruitName";
+  String imageFS = "image";
+  String qtyFS = "qty";
+  String idFS = "id";
+  String id;
+  String fruitName;
+  String image;
+  num qty;
+
+  Fruit(this.id, this.fruitName, this.qty);
+
+  Fruit.fromSnap(DocumentSnapshot snap) {
+    Map<String, dynamic> map = snap.data;
+    this.id = snap.documentID;
+    this.fruitName = map[fruitNameFS];
+    this.qty = map[qtyFS];
+    this.image = map[imageFS];
+  }
+
+  Map<String, dynamic> toMap() =>
+      {idFS: id, fruitNameFS: fruitName, qtyFS: qty, imageFS : image};
+
+  Future<void> uploadToFirestore(Transaction transaction) async {
+    await transaction.set(this.ref, toMap());
+  }
+
+  void addFruit(num qty) {
+    this.qty += qty;
+    TestObjects().fruits.forEach((fruit){
+      if (this.id == fruit.id){
+        fruit.qty += qty;
+      }
+    });
+  }
+
+  void removeFruit(num qty) {
+    this.qty -= qty;
+    if (qty < 0) throw LessThanZeroFruits();
+    TestObjects().fruits.forEach((fruit){
+      if (this.id == fruit.id){
+        fruit.qty -= qty;
+      }
+    });
+  }
+
+  String toString(){
+    return "${this.fruitName} has ${this.qty} pcs";
+  }
+}
+
+class LessThanZeroFruits implements Exception {
+  String message = "Not enough Fruits";
+  LessThanZeroFruits({this.message});
+}
